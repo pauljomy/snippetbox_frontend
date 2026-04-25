125441ed266a6852a75630a7dc572f062eb6b3a7
Paul Jomy <pauljomy@users.noreply.github.com>
2026-04-25 18:15:50 +0530

    upadate few componets to fetch data from backend

--- src/components/LatestPosts.jsx
+++ src/components/LatestPosts.jsx
@@ -1,11 +1,14 @@
 import SinglePost from "./SinglePost";
+import { Link } from "react-router-dom";
 
 const LatestPosts = ({ snippets }) => {
   return (
     <div>
       {snippets.map((snippet) => (
         <div key={snippet.ID}>
-          <SinglePost snippet={snippet} />
+          <Link to={`/snippet/view/${snippet.ID}`}>
+            <SinglePost snippet={snippet} />
+          </Link>
         </div>
       ))}
     </div>
