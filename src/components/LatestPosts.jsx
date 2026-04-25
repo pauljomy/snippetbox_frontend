d204327a2e5d975bd501614e1767a63c32fc1882
Paul Jomy <pauljomy@users.noreply.github.com>
2026-04-25 19:00:28 +0530

    updated the date format

--- src/components/LatestPosts.jsx
+++ src/components/LatestPosts.jsx
@@ -2,6 +2,23 @@ import SinglePost from "./SinglePost";
 import { Link } from "react-router-dom";
 
 const LatestPosts = ({ snippets }) => {
+  const formatDate = (dateStr) => {
+    const date = new Date(dateStr);
+    return (
+      date.toLocaleDateString("en-GB", {
+        day: "2-digit",
+        month: "short",
+        year: "numeric",
+      }) +
+      " at " +
+      date.toLocaleTimeString("en-GB", {
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: false,
+      })
+    );
+  };
+
   return (
     <div className="mt-10 max-w-3xl border border-gray-200">
       <div className="grid grid-cols-[2fr_2fr_1fr] px-5 py-3 font-bold text-gray-700 border-b border-gray-200 bg-gray-50">
@@ -14,7 +31,7 @@ const LatestPosts = ({ snippets }) => {
           <Link to={`/snippet/view/${snippet.ID}`}>
             <div className="grid grid-cols-[2fr_2fr_1fr] px-5 py-3 border-b border-gray-200 ">
               <span className="text-green-600">{snippet.Title}</span>
-              <span>{new Date(snippet.Created).toLocaleString()}</span>
+              <span>{formatDate(snippet.Created)}</span>
               <span className=" text-right">#{snippet.ID}</span>
             </div>
           </Link>
